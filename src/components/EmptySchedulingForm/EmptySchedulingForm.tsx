6f314880ec2d1587157719f8d5d61d479a192025
Martin <david.martin6@ascension.org>
2023-11-26 16:06:31 -0600

    updated empty form

--- src/components/EmptySchedulingForm/EmptySchedulingForm.tsx
+++ src/components/EmptySchedulingForm/EmptySchedulingForm.tsx
@@ -11,17 +11,14 @@ const EmptySchedulingForm: React.FC<EmptySchedulingFormProps> = ({procedureId}) 
 
     const [message, setMessage] = useState('')
     const [showDirections, setShowDirections] = useState(false);
-    const [showFullScreen, setShowFullScreen] = useState('showfullscreen')
 
     useEffect(()=> {
         if (procedureId === -1) {
             setMessage('Disabled SSM Procedure Scheduling Form');
             setShowDirections(false);
-            setShowFullScreen('showfullscreen')
         } else {
             setMessage('Enabled and prepopulated SSM Procedure Scheduling Form');
             setShowDirections(true);
-            setShowFullScreen('');
         } 
 
     })
@@ -31,7 +28,7 @@ const EmptySchedulingForm: React.FC<EmptySchedulingFormProps> = ({procedureId}) 
             {showDirections && <div className='empty-scheduling-form-directions'>
                 <h2>Fill Out Form Below To Schedule Selected Procedure Time</h2>
             </div>}
-            <div className={`emptyschedulingform-message ${showFullScreen}`}> 
+            <div className={`emptyschedulingform-message`}> 
                 <h2>{message}</h2>
             </div>
         </div>
